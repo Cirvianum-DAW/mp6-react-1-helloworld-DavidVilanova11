c02198d5490b9d83504c34e62b62bce0393b02e1
daw25 <navjashan.singh@cirvianum.cat>
2024-04-17 17:58:17 +0200

    Refactor App component to include Form component and manage places state

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,15 @@ import React from "react";
 import { useState } from "react";
 import Form from "./components/Form";
 
-
 function App() {
   const [tipusEstudiant, setTipusEstudiant] = useState("no-graduat"); // Afegim la variable d'estat tipusEstudiant
+  const [places, setPlaces] = useState(100);
   const handleChange = (e) => {
     setTipusEstudiant(e.target.value);
   }; // Afegim la funció que actualitza el valor de la variable d'estat tipusEstudiant
+  const setPlacesDisponibles = (updatedPlaces) => {
+    setPlaces(updatedPlaces);
+  };
   return (
     <div className="flex flex-col items-center justify-center gap-5 h-screen">
       <div className="tipusEstudiant">
@@ -21,10 +24,13 @@ function App() {
           <option value="graduat">Graduat</option>
         </select>
       </div>
-      <Form tipusEstudiantSelect={tipusEstudiant} />
+      <Form
+        tipusEstudiantSelect={tipusEstudiant}
+        setPlacesDisponibles={setPlacesDisponibles}
+        placesActuals={places}
+      />{" "}
     </div>
   );
 }
-
 
 export default App;
